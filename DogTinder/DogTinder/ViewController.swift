3f33268cb0268be1ab625ebe2bea45241bc149f1
Joshua Neill (s5008067) <s5008067@mac38582.student.bournemouth.ac.uk>
2018-11-20 10:08:21 +0000

    new project

---
+++ DogTinder/DogTinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogTinder
+//
+//  Created by Joshua Neill (s5008067) on 20/11/2018.
+//  Copyright © 2018 Joshua Neill (s5008067). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
